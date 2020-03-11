43bb879fa1d556696a6e4a5a67c3d708fb16a2d3
jimnor0xF <xanthos444@gmail.com>
2020-03-11 23:10:25 +0100

    Done

--- quiz.go
+++ quiz.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	fileFlagPtr := flag.String("csv_file", "problems.csv", "file path to problems")
 
-	//timeFlagPtr := flag.Int("timer", 30, "timer for questions")
+	timeFlagPtr := flag.Int("timer", 30, "timer for questions")
 
 	flag.Parse()
 
@@ -39,43 +39,38 @@ func main() {
 		problems[i].a = record[1]
 	}
 
-	ticker := time.NewTicker(1 * time.Second)
 	var correctAnswers int
-	go runQuiz(correctAnswers, problems)
-	go tickTime(ticker)
-	time.Sleep(30 * time.Second)
-	ticker.Stop()
+	timer := time.NewTimer(time.Duration(*timeFlagPtr) * time.Second)
 
-	fmt.Printf("Score: %d/%d\n", correctAnswers, len(problems))
-
-}
-
-func runQuiz(correctAnswers int, problems []question) {
+Loop:
 	for _, problem := range problems {
-
+		answerChan := make(chan string)
 		fmt.Printf("What is %s?\n", problem.q)
 
-		var input string
-		_, err := fmt.Scanf("%s\n", &input)
+		go func() {
+			var input string
+			_, err := fmt.Scanf("%s\n", &input)
 
-		if err != nil {
-			log.Fatal(err)
-		}
+			if err != nil {
+				log.Fatal(err)
+			}
 
-		if input == problem.a {
-			correctAnswers++
+			answerChan <- input
+
+		}()
+
+		select {
+		case <-timer.C:
+			break Loop
+		case answer := <-answerChan:
+			if answer == problem.a {
+				correctAnswers++
+
+			}
 		}
 
 	}
-
-}
-
-func tickTime(ticker *time.Ticker) {
-	i := 1
-	for t := range ticker.C {
-		i = i + 1
-		fmt.Println("Tick at", t)
-	}
+	fmt.Printf("Score: %d/%d\n", correctAnswers, len(problems))
 
 }
 
